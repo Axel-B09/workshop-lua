2fb1a2422461e92e0995a1662915dad8f4a12ba9
River <axel.boqueho@epitech.eu>
2023-02-08 15:54:24 +0100

    [ADD] ex 2 to 6

---
+++ ex5.lua
@@ -0,0 +1,9 @@
+function FileManipulation(filepath)
+    for str in io.lines(filepath) do
+        if (#str % 2 == 0) then
+            print(str)
+        end
+    end
+end
+
+FileManipulation('5.txt')
