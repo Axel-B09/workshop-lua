9894ff50472e462e30d993d5b3e99e40a24ba26c
River <axel.boqueho@epitech.eu>
2023-02-08 14:17:31 +0100

    [ADD] ex1

---
+++ file.lua
@@ -0,0 +1,15 @@
+function Count()
+    for n = 1, 100, 1 do
+        if (n % 3 == 0) then
+            if (n % 5 == 0) then
+                print("ThreeFive")
+            else
+                print("Three")
+            end
+        elseif (n % 5 == 0) then
+            print("Five")
+        end
+    end
+end
+
+Count()
